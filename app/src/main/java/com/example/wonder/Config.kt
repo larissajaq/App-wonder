372057394ea933918642ab3a10eeedbc1acf39f3
wonder <larissajaq.designer@gmail.com>
2019-04-03 16:47:01 -0300

    Tentativa SharedPreferences

--- app/src/main/java/com/example/wonder/Config.kt
+++ app/src/main/java/com/example/wonder/Config.kt
@@ -1,69 +1,75 @@
 package com.example.wonder
 
-import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.preference.PreferenceManager
 import android.widget.Button
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_config.*
+import kotlin.properties.Delegates
 
 class Config : AppCompatActivity() {
 
-   // val PREFS_FILENAME = "com.teamtreehouse.colorsarefun.prefs"
-   // val BUTTON_COLOR = "button_color"
-   // var prefs: SharedPreferences? = null
+    lateinit var button_change: Button
+    var button_background: Int by Delegates.notNull<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_config)
+        val sharedPreference: SharedPreference = SharedPreference(this)
 
-      //  prefs = this.getSharedPreferences(PREFS_FILENAME, 0)
-
-      //  val prefs = this.getSharedPreferences(PREFS_FILENAME, 0)
-    //    val bgColor = prefs!!.getInt(BUTTON_COLOR, 1)
-
-    var button_background : Int = 1;
-
-    var button_change = findViewById(R.id.vibrar) as Button;
-
-    vibrar.setOnClickListener {
-        if(button_background==2){
-            button_change.setBackgroundResource(R.drawable.roundedbutton);
-            button_background=1;
-        } else if(button_background==1){
-            button_change.setBackgroundResource(R.drawable.roundedbuttontwo);
-            button_background=2;
+        if (sharedPreference.getValueInt("intval") != 0) {
+            button_background = sharedPreference.getValueInt("intval")
+            Toast.makeText(this@Config, "Data Retrieved", Toast.LENGTH_SHORT).show()
+        } else {
+            button_background = 1
+            Toast.makeText(this@Config, "Data Retrie", Toast.LENGTH_SHORT).show()
         }
-       // val editor = prefs!!.edit()
-       // editor.putInt(BUTTON_COLOR, button_background)
-       // editor.apply()
+
+        button_change = findViewById(R.id.vibrar)
+
+        vibrar.setOnClickListener {
+            if (button_background == 2) {
+                button_change.setBackgroundResource(R.drawable.roundedbutton);
+                button_background = 1;
+                sharedPreference.save("intval", 1)
+                Toast.makeText(this@Config, "Data Stored", Toast.LENGTH_SHORT).show()
+            } else if (button_background == 1) {
+                button_change.setBackgroundResource(R.drawable.roundedbuttontwo);
+                button_background = 2;
+                sharedPreference.save("intval", 2)
+                Toast.makeText(this@Config, "Data Saved", Toast.LENGTH_SHORT).show()
+            }
+
+
+
+                var button_backgroundtwo: Int = 1;
+
+                var button_changetwo = findViewById(R.id.vento) as Button;
+
+                vento.setOnClickListener {
+                    if (button_backgroundtwo == 2) {
+                        button_changetwo.setBackgroundResource(R.drawable.roundedbutton);
+                        button_backgroundtwo = 1;
+                    } else if (button_backgroundtwo == 1) {
+                        button_changetwo.setBackgroundResource(R.drawable.roundedbuttontwo);
+                        button_backgroundtwo = 2;
+                    }
+                }
+
+                var button_backgroundthree: Int = 1;
+
+                var button_changethree = findViewById(R.id.toque) as Button;
+
+                toque.setOnClickListener {
+                    if (button_backgroundthree == 2) {
+                        button_changethree.setBackgroundResource(R.drawable.roundedbutton);
+                        button_backgroundthree = 1;
+                    } else if (button_backgroundthree == 1) {
+                        button_changethree.setBackgroundResource(R.drawable.roundedbuttontwo);
+                        button_backgroundthree = 2;
+                    }
+                }
+            }
+        }
     }
-        var button_backgroundtwo : Int = 1;
 
-        var button_changetwo = findViewById(R.id.vento) as Button;
-
-        vento.setOnClickListener {
-            if(button_backgroundtwo==2){
-                button_changetwo.setBackgroundResource(R.drawable.roundedbutton);
-                button_backgroundtwo=1;
-            } else if(button_backgroundtwo==1){
-                button_changetwo.setBackgroundResource(R.drawable.roundedbuttontwo);
-                button_backgroundtwo=2;
-            }
-        }
-
-        var button_backgroundthree : Int = 1;
-
-        var button_changethree = findViewById(R.id.toque) as Button;
-
-        toque.setOnClickListener {
-            if(button_backgroundthree==2){
-                button_changethree.setBackgroundResource(R.drawable.roundedbutton);
-                button_backgroundthree=1;
-            } else if(button_backgroundthree==1){
-                button_changethree.setBackgroundResource(R.drawable.roundedbuttontwo);
-                button_backgroundthree=2;
-            }
-        }
-}
-}
