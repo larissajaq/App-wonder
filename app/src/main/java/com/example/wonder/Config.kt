d3f7fab0fbccf98574b1262a2b0f15318efc366d
larissajaq <larissajaq.designer@gmail.com>
2019-04-05 11:16:37 -0300

    Configurações e cards funcionando

--- app/src/main/java/com/example/wonder/Config.kt
+++ app/src/main/java/com/example/wonder/Config.kt
@@ -2,75 +2,119 @@ package com.example.wonder
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_config.*
 import kotlin.properties.Delegates
 
 class Config : AppCompatActivity() {
 
-    lateinit var button_change: Button
-    var button_background: Int by Delegates.notNull<Int>()
+    lateinit var button_vibrar: Button
+    var button_vibrarbackground: Int by Delegates.notNull<Int>()
+    lateinit var button_vento: Button
+    var button_ventobackground: Int by Delegates.notNull<Int>()
+    lateinit var button_toque: Button
+    var button_toquebackground: Int by Delegates.notNull<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_config)
         val sharedPreference: SharedPreference = SharedPreference(this)
+        val intval: Int = sharedPreference.getValueInt("intval")
+        val intvalvento: Int = sharedPreference.getValueInt("intvalvento")
+        val intvaltoque: Int = sharedPreference.getValueInt("intvaltoque")
 
-        if (sharedPreference.getValueInt("intval") != 0) {
-            button_background = sharedPreference.getValueInt("intval")
-            Toast.makeText(this@Config, "Data Retrieved", Toast.LENGTH_SHORT).show()
-        } else {
-            button_background = 1
-            sharedPreference.save("intval", 1)
-            Toast.makeText(this@Config, "Data Retrie", Toast.LENGTH_SHORT).show()
-        }
+        button_vibrar = findViewById(R.id.vibrar)
+        button_vibrarbackground = intval
 
-        button_change = findViewById(R.id.vibrar)
+        this.changeButtonState(button_vibrar, button_vibrarbackground) // serve para verificar o estado atual do botão conforme o valor salvo
 
         vibrar.setOnClickListener {
-            if (button_background == 2) {
-                button_change.setBackgroundResource(R.drawable.roundedbutton);
-                button_background = 1;
-                sharedPreference.save("intval", 1)
-                Toast.makeText(this@Config, "Data Stored", Toast.LENGTH_SHORT).show()
-            } else if (button_background == 1) {
-                button_change.setBackgroundResource(R.drawable.roundedbuttontwo);
-                button_background = 2;
-                sharedPreference.save("intval", 2)
-                Toast.makeText(this@Config, "Data Saved", Toast.LENGTH_SHORT).show()
-            }
-
-
-
-                var button_backgroundtwo: Int = 1;
-
-                var button_changetwo = findViewById(R.id.vento) as Button;
-
-                vento.setOnClickListener {
-                    if (button_backgroundtwo == 2) {
-                        button_changetwo.setBackgroundResource(R.drawable.roundedbutton);
-                        button_backgroundtwo = 1;
-                    } else if (button_backgroundtwo == 1) {
-                        button_changetwo.setBackgroundResource(R.drawable.roundedbuttontwo);
-                        button_backgroundtwo = 2;
-                    }
-                }
-
-                var button_backgroundthree: Int = 1;
-
-                var button_changethree = findViewById(R.id.toque) as Button;
-
-                toque.setOnClickListener {
-                    if (button_backgroundthree == 2) {
-                        button_changethree.setBackgroundResource(R.drawable.roundedbutton);
-                        button_backgroundthree = 1;
-                    } else if (button_backgroundthree == 1) {
-                        button_changethree.setBackgroundResource(R.drawable.roundedbuttontwo);
-                        button_backgroundthree = 2;
-                    }
-                }
+            if (button_vibrarbackground == 2) {
+                button_vibrarbackground = 1;
+                this.changeButtonState(button_vibrar, button_vibrarbackground)
+                sharedPreference.save("intval", button_vibrarbackground)
+            } else {
+                button_vibrarbackground = 2;
+                this.changeButtonState(button_vibrar, button_vibrarbackground)
+                sharedPreference.save("intval", button_vibrarbackground)
             }
         }
-    }
+
+            button_vento = findViewById(R.id.vento)
+            button_ventobackground = intvalvento
+
+            this.changeButtonState(button_vento, button_ventobackground) // serve para veificar o estado atual do botão conforme o valor salvo
+
+            vento.setOnClickListener {
+                if (button_ventobackground == 2) {
+                    button_ventobackground = 1;
+                    this.changeButtonState(button_vento, button_ventobackground)
+                    sharedPreference.save("intvalvento", button_ventobackground)
+                } else {
+                    button_ventobackground = 2;
+                    this.changeButtonState(button_vento, button_ventobackground)
+                    sharedPreference.save("intvalvento", button_ventobackground)
+                }
+            }
+
+                button_toque = findViewById(R.id.toque)
+                button_toquebackground = intvaltoque
+
+                this.changeButtonState(button_toque, button_toquebackground) // serve para veificar o estado atual do botão conforme o valor salvo
+
+                toque.setOnClickListener {
+                    if (button_toquebackground == 2) {
+                        button_toquebackground = 1;
+                        this.changeButtonState(button_toque, button_toquebackground)
+                        sharedPreference.save("intvaltoque", button_toquebackground)
+                    } else {
+                        button_toquebackground = 2;
+                        this.changeButtonState(button_toque, button_toquebackground)
+                        sharedPreference.save("intvaltoque", button_toquebackground)
+                    }
+                }
+
+//
+//
+//                var button_backgroundtwo: Int = 1;
+//
+//                var button_changetwo = findViewById(R.id.vento) as Button;
+//
+//                vento.setOnClickListener {
+//                    if (button_backgroundtwo == 2) {
+//                        button_changetwo.setBackgroundResource(R.drawable.roundedbutton);
+//                        button_backgroundtwo = 1;
+//                    } else if (button_backgroundtwo == 1) {
+//                        button_changetwo.setBackgroundResource(R.drawable.roundedbuttontwo);
+//                        button_backgroundtwo = 2;
+//                    }
+//                }
+//
+//                var button_backgroundthree: Int = 1;
+//
+//                var button_changethree = findViewById(R.id.toque) as Button;
+//
+//                toque.setOnClickListener {
+//                    if (button_backgroundthree == 2) {
+//                        button_changethree.setBackgroundResource(R.drawable.roundedbutton);
+//                        button_backgroundthree = 1;
+//                    } else if (button_backgroundthree == 1) {
+//                        button_changethree.setBackgroundResource(R.drawable.roundedbuttontwo);
+//                        button_backgroundthree = 2;
+//                    }
+//                }
+            }
+
+
+        private fun changeButtonState(button: Button, intval: Int) {
+            if (intval == 2) {
+                button.setBackgroundResource(R.drawable.roundedbuttontwo)
+            }
+            else {
+                button.setBackgroundResource(R.drawable.roundedbutton)
+            }
+            Log.d("changeButtonState", intval.toString())
+        }
+}
 
--- app/src/main/java/com/example/wonder/MainActivity.kt
+++ app/src/main/java/com/example/wonder/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.wonder
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,27 +14,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         cardPorquinhos.setOnClickListener(clickListener)
+        cardPeter.setOnClickListener(clickListener)
+        cardCinderela.setOnClickListener(clickListener)
+        cardAladdin.setOnClickListener(clickListener)
     }
 
     fun ClickButton2(view: View) {
         var intent: Intent = Intent(this, Config::class.java)
         startActivity(intent)
     }
+    fun ClickLog(view: View) {
+        Log.d("log", "clicked");
+    }
     private val clickListener: View.OnClickListener = View.OnClickListener { view ->
         when (view.id) {
-            R.id.cardPorquinhos -> ClickLivro()
-            R.id.cardPeter -> ClickToast(view)
+            R.id.cardPorquinhos -> this.ClickLivro()
+            R.id.cardPeter -> this.ClickToast(view)
+            R.id.cardCinderela -> this.ClickToast(view)
+            R.id.cardAladdin -> this.ClickToast(view)
         }
     }
     private fun ClickLivro() {
         val intent = Intent(this, Livro::class.java)
         startActivity(intent)
     }
-    fun ClickToast(view: View) {
-        val text = "Em breve"
-        val duration = Toast.LENGTH_SHORT
-        val toast = Toast.makeText(applicationContext, text, duration)
-        toast.show()
+    private fun ClickToast(view: View) {
+        Toast.makeText(this@MainActivity, "Em breve", Toast.LENGTH_SHORT).show()
     }
+
     }
 
